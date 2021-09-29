22f0ff10df48fe2bb4a4de656a80a29df0650fd3
Senol Atac <senol.atac.1@gmail.com>
2021-09-29 14:41:32 +0300

    initial

--- src/main/java/com/example/ebscaledemo/controller/DemoController.java
+++ src/main/java/com/example/ebscaledemo/controller/DemoController.java
@@ -4,7 +4,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
+import java.net.URL;
 import java.net.UnknownHostException;
 
 /**
@@ -19,5 +23,30 @@ public class DemoController
     public ResponseEntity<?> getIpAddress() throws UnknownHostException
     {
         return ResponseEntity.ok(InetAddress.getLocalHost().getHostAddress());
+    }
+
+    @GetMapping("/api/external-ip")
+    public ResponseEntity<?> getExternalIpAddress() throws Exception
+    {
+        return ResponseEntity.ok(getIp());
+    }
+
+    private static String getIp() throws Exception {
+        URL whatismyip = new URL("http://checkip.amazonaws.com");
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new InputStreamReader(
+                    whatismyip.openStream()));
+            String ip = in.readLine();
+            return ip;
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
